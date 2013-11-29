230f097040fd51a80d5ee9257c84cb4b47491bc5
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-11-29 12:18:02 -0700

    Initial commit of new changes

--- build/support/002_configuration/git.rb
+++ build/support/002_configuration/git.rb
@@ -1,5 +1,6 @@
 configs ={
   :git => {
+    :provider => missing("provider",__FILE__),
     :user => missing("user",__FILE__),
     :remotes => potentially_change("remotes",__FILE__),
     :repo => 'prep' 
