ba3c82343f6946bbd0abb9796e51063672fd6299
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-10-28 06:58:02 -0600

    Fixed the utility

--- build/automation/git_utils.rb
+++ build/automation/git_utils.rb
@@ -43,8 +43,7 @@ command
 
     def pick_item_from(items,prompt)
       items.each_with_index{|item,index| p "#{index + 1} - #{item}"}
-      p prompt
-      index = gets.chomp.to_i
+      index = ask(prompt).to_i
       return index == 0 ? "": items[index-1]
     end
 
