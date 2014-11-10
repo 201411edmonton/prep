3caaab0e3c500bdaf6070540d8923b3fd9c8e51c
Adrienne Paton <apaton@linus7.com>
2014-11-10 15:54:23 -0700

    Pushing new changes

--- source/prep/movies/MovieLibrary.cs
+++ source/prep/movies/MovieLibrary.cs
@@ -4,89 +4,132 @@ using prep.utility;
 
 namespace prep.movies
 {
-  public class MovieLibrary
-  {
-    IList<Movie> movies;
-
-    public MovieLibrary(IList<Movie> list_of_movies)
+    public class MovieLibrary
     {
-      this.movies = list_of_movies;
-    }
+        private IList<Movie> movies;
 
-    public IEnumerable<Movie> all_movies()
-    {
-      return movies.one_at_a_time();
-    }
+        public MovieLibrary(IList<Movie> list_of_movies)
+        {
+            this.movies = list_of_movies;
+        }
 
-    public void add(Movie movie)
-    {
-      if (already_contains(movie)) return;
-      movies.Add(movie);
-    }
+        public IEnumerable<Movie> all_movies()
+        {
+            return movies.one_at_a_time();
+        }
 
-    bool already_contains(Movie movie)
-    {
-      return movies.Contains(movie);
-    }
+        public void add(Movie movie)
+        {
+            if (already_contains(movie)) return;
+            movies.Add(movie);
+        }
 
-    public IEnumerable<Movie> all_movies_published_by_pixar()
-    {
-      throw new NotImplementedException();
-    }
+        private bool already_contains(Movie movie)
+        {
+            return movies.Contains(movie);
+        }
 
-    public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
-    {
-      throw new NotImplementedException();
-    }
+        private List<Movie> movies_published_by(ProductionStudio studio)
+        {
+            var matches = new List<Movie>();
+            foreach (var movie in movies)
+            {
+                if (movie.production_studio == studio)
+                    matches.Add(movie);
+            }
+            return matches;
+        }
 
-    public IEnumerable<Movie> all_movies_not_published_by_pixar()
-    {
-      throw new NotImplementedException();
-    }
+        public IEnumerable<Movie> all_movies_published_by_pixar()
+        {
+            return movies_published_by(ProductionStudio.Pixar);
+        }
 
-    public IEnumerable<Movie> all_movies_published_after(int year)
-    {
-      throw new NotImplementedException();
-    }
+        public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
+        {
+            var set = movies_published_by(ProductionStudio.Pixar);
+            set.AddRange(movies_published_by(ProductionStudio.Disney));
+            return set;
+        }
 
-    public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
-    {
-      throw new NotImplementedException();
-    }
+        public IEnumerable<Movie> all_movies_not_published_by_pixar()
+        {
+            var matches = new List<Movie>();
+            foreach (var movie in movies)
+            {
+                if (movie.production_studio != ProductionStudio.Pixar)
+                    matches.Add(movie);
+            }
+            return matches;
+        }
 
-    public IEnumerable<Movie> all_kid_movies()
-    {
-      throw new NotImplementedException();
-    }
+        public IEnumerable<Movie> all_movies_published_after(int year)
+        {
+            var matches = new List<Movie>();
+            foreach (var movie in movies)
+            {
+                if (movie.date_published.Year > year)
+                    matches.Add(movie);
+            }
+            return matches;
+        }
 
-    public IEnumerable<Movie> all_action_movies()
-    {
-      throw new NotImplementedException();
-    }
+        public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
+        {
+            var matches = new List<Movie>();
+            foreach (var movie in movies)
+            {
+                if (movie.date_published.Year >= startingYear && movie.date_published.Year <= endingYear)
+                    matches.Add(movie);
+            }
+            return matches;
+        }
 
-    public IEnumerable<Movie> sort_all_movies_by_title_descending()
-    {
-      throw new NotImplementedException();
-    }
+        public IEnumerable<Movie> all_kid_movies()
+        {
+            var matches = new List<Movie>();
+            foreach (var movie in movies)
+            {
+                if (movie.genre == Genre.kids)
+                    matches.Add(movie);
+            }
+            return matches;
+        }
 
-    public IEnumerable<Movie> sort_all_movies_by_title_ascending()
-    {
-      throw new NotImplementedException();
-    }
+        public IEnumerable<Movie> all_action_movies()
+        {
+            var matches = new List<Movie>();
+            foreach (var movie in movies)
+            {
+                if (movie.genre == Genre.action)
+                    matches.Add(movie);
+            }
+            return matches;
+        }
 
-    public IEnumerable<Movie> sort_all_movies_by_movie_studio_and_year_published()
-    {
-      throw new NotImplementedException();
-    }
+        public IEnumerable<Movie> sort_all_movies_by_title_descending()
+        {
+            throw new NotImplementedException();
+        }
 
-    public IEnumerable<Movie> sort_all_movies_by_date_published_descending()
-    {
-      throw new NotImplementedException();
-    }
+        public IEnumerable<Movie> sort_all_movies_by_title_ascending()
+        {
+            throw new NotImplementedException();
+        }
 
-    public IEnumerable<Movie> sort_all_movies_by_date_published_ascending()
-    {
-      throw new NotImplementedException();
+        public IEnumerable<Movie> sort_all_movies_by_movie_studio_and_year_published()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> sort_all_movies_by_date_published_descending()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> sort_all_movies_by_date_published_ascending()
+        {
+            throw new NotImplementedException();
+        }
     }
-  }
 }
