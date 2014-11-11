fee97aadfa2949c6e30fcd90f6451b30d7f19c6d
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-11-11 14:54:15 -0700

    Updated

--- source/prep/matching/IProvideAccessToCreateMatchers.cs
+++ source/prep/matching/IProvideAccessToCreateMatchers.cs
@@ -1,12 +1,7 @@
 namespace prep.matching
 {
-  public interface ICreateAMatcher<in ItemToMatch, out AttributeType>
+  public interface IProvideAccessToCreateMatchers<in ItemToMatch, out AttributeType>
   {
     IMatchA<ItemToMatch> create_match(IMatchA<AttributeType> value_matcher);
-  }
-
-  public interface IProvideAccessToCreateMatchers<in ItemToMatch, out AttributeType>
-    : ICreateAMatcher<ItemToMatch, AttributeType>
-  {
   }
 }
--- source/prep/matching/MatchCreationExtensions.cs
+++ source/prep/matching/MatchCreationExtensions.cs
@@ -6,33 +6,33 @@ namespace prep.matching
   public static class MatchCreationExtensions
   {
     public static IMatchA<ItemToMatch> equal_to<ItemToMatch, AttributeType>(
-      this ICreateAMatcher<ItemToMatch, AttributeType> extension_point, params AttributeType[] values)
+      this IProvideAccessToCreateMatchers<ItemToMatch, AttributeType> extension_point, params AttributeType[] values)
     {
       return create_from_value_matcher(extension_point, new EqualToAny<AttributeType>(values));
     }
 
     public static IMatchA<ItemToMatch> greater_than<ItemToMatch, AttributeType>(
-      this ICreateAMatcher<ItemToMatch, AttributeType> extension_point, AttributeType value)
+      this IProvideAccessToCreateMatchers<ItemToMatch, AttributeType> extension_point, AttributeType value)
       where AttributeType : IComparable<AttributeType>
     {
       return create_from_value_matcher(extension_point, new GreaterThan<AttributeType>(value));
     }
 
     public static IMatchA<ItemToMatch> between<ItemToMatch, AttributeType>(
-      this ICreateAMatcher<ItemToMatch, AttributeType> extension_point, AttributeType start,
+      this IProvideAccessToCreateMatchers<ItemToMatch, AttributeType> extension_point, AttributeType start,
       AttributeType end) where AttributeType : IComparable<AttributeType>
     {
       return create_from_value_matcher(extension_point, new IsBetween<AttributeType>(start, end));
     }
 
     public static IMatchA<ItemToMatch> create_from_condition<ItemToMatch, AttributeType>(
-      this ICreateAMatcher<ItemToMatch, AttributeType> extension_point, Condition<ItemToMatch> condition)
+      this IProvideAccessToCreateMatchers<ItemToMatch, AttributeType> extension_point, Condition<ItemToMatch> condition)
     {
       return new ConditionalMatch<ItemToMatch>(condition);
     }
 
     public static IMatchA<ItemToMatch> create_from_value_matcher<ItemToMatch, AttributeType>(
-      this ICreateAMatcher<ItemToMatch, AttributeType> extension_point, IMatchA<AttributeType> value_matcher)
+      this IProvideAccessToCreateMatchers<ItemToMatch, AttributeType> extension_point, IMatchA<AttributeType> value_matcher)
     {
       return extension_point.create_match(value_matcher);
     }
