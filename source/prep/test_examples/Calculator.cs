fa06b0fd54bbd9a8ac5efb552183f5686001620f
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-11-11 20:08:29 -0700

    Updated

--- source/prep/test_examples/Calculator.cs
+++ source/prep/test_examples/Calculator.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Data;
 
 namespace prep.test_examples
 {
   public class Calculator
   {
+    public Calculator(IDbConnection connection)
+    {
+    }
+
     public int add(int first, int second)
     {
         if (first < 0 || second > 0)
--- source/prep/test_examples/CalculatorSpecs.cs
+++ source/prep/test_examples/CalculatorSpecs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
 using Machine.Specifications;
@@ -10,21 +11,31 @@ namespace prep.test_examples
   {
     public abstract class concern : Observes<Calculator>
     {
-      
+      //Arrange
+      Establish c = () =>
+      {
+        connection = depends.on<IDbConnection>();
+      };
+
+      protected static IDbConnection connection;
     }
 
     public class when_adding : concern
     {
       public class two_positive_numbers
       {
+        //Act
         Because b = () =>
           result = sut.add(2, 3);
 
+        //Assert
         It returns_the_sum = () =>
           result.ShouldEqual(5);
 
-        static int result;
+        It opens_a_connection_to_the_database = () =>
+          connection.received(x => x.Open());
 
+        static int result;
       }
 
       public class a_negative_to_a_positive
@@ -32,6 +43,9 @@ namespace prep.test_examples
         Because b = () =>
           spec.catch_exception(() => sut.add(2, -3));
 
+        It does_not_use_the_connection = () =>
+          connection.never_received(x => x.Open());
+          
         It cannot_do_the_addition = () =>
           spec.exception_thrown.ShouldBeAn<ArgumentException>();
       }
