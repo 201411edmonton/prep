4a8ff0fa6a616aa978ec0b7656687c899a2ccdeb
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-11-11 20:47:33 -0700

    Updated

--- source/prep/test_examples/Calculator.cs
+++ source/prep/test_examples/Calculator.cs
@@ -1,30 +1,56 @@
 ﻿using System;
 using System.Data;
+using System.Security;
+using System.Threading;
 
 namespace prep.test_examples
 {
-    public class Calculator
+  public interface ICanPowerDown
+  {
+    void shut_off();
+  }
+
+  public interface ICalculate 
+  {
+    int add(int first, int second);
+  }
+
+  public class Calculator : ICalculate, ICanPowerDown
+  {
+    IDbConnection connection;
+    int offset;
+
+    public Calculator(IDbConnection connection, int offset, int offset2)
     {
-        private IDbConnection connection;
-
-        public Calculator(IDbConnection connection)
-        {
-            this.connection = connection;
-        }
-
-        public int add(int first, int second)
-        {
-            ensure_positive_arguments(first, second);
-            connection.Open();
-            var command = connection.CreateCommand();
-            command.ExecuteNonQuery();
-            return first + second;
-        }
-
-        private static void ensure_positive_arguments(int first, int second)
-        {
-            if (first < 0 || second < 0)
-                throw new ArgumentException();
-        }
+      this.connection = connection;
+      this.offset = offset;
     }
+
+    public int add(int first, int second)
+    {
+      ensure_positive_arguments(first, second);
+
+      using (connection)
+      using (var command = connection.CreateCommand())
+      {
+        connection.Open();
+        command.ExecuteNonQuery();
+      }
+
+      return first + second + offset;
+    }
+
+    static void ensure_positive_arguments(int first, int second)
+    {
+      if (first < 0 || second < 0)
+        throw new ArgumentException();
+    }
+
+    public void shut_off()
+    {
+      if (Thread.CurrentPrincipal.IsInRole("blah")) return;
+
+      throw new SecurityException("You cannot do this");
+    }
+  }
 }
--- source/prep/test_examples/CalculatorSpecs.cs
+++ source/prep/test_examples/CalculatorSpecs.cs
@@ -1,36 +1,85 @@
 ﻿using System;
 using System.Data;
+using System.Security;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Threading;
 using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
 using Machine.Specifications;
+using prep.matching;
+using Rhino.Mocks;
 
 namespace prep.test_examples
 {
   [Subject(typeof(Calculator))]
   public class CalculatorSpecs
   {
-    public abstract class concern : Observes<Calculator>
+    public abstract class concern_for_calculation : Observes<ICalculate, Calculator>
     {
       //Arrange
       Establish c = () =>
       {
         connection = depends.on<IDbConnection>();
+        command = fake.an<IDbCommand>();
+
+        connection.setup(x => x.CreateCommand()).Return(command);
       };
 
       protected static IDbConnection connection;
+      protected static IDbCommand command;
     }
 
-    public class when_adding : concern
+    public abstract class concern_for_powering_down : Observes<ICanPowerDown, Calculator>
     {
-      public class two_positive_numbers
+      
+    }
+    public class when_shutting_off_the_calculator : concern_for_powering_down
+    {
+      public class and_they_are_in_the_correct_security_group
       {
         Establish c = () =>
         {
-          command = fake.an<IDbCommand>();
+          principal = fake.an<IPrincipal>();
+          principal.setup(x => x.IsInRole(Arg<string>.Is.Anything)).Return(true);
 
-          connection.setup(x => x.CreateCommand()).Return(command);
+          spec.change(() => Thread.CurrentPrincipal).to(principal);
         };
 
+        Because b = () =>
+          sut.shut_off();
+
+        It allows_them_to_shut_if_off = () =>
+        {
+        };
+
+        static IPrincipal principal;
+      }
+
+      public class and_they_are_not_in_the_correct_security_group
+      {
+        Establish c = () =>
+        {
+          principal = fake.an<IPrincipal>();
+          principal.setup(x => x.IsInRole(Arg<string>.Is.Anything)).Return(false);
+
+          spec.change(() => Thread.CurrentPrincipal).to(principal);
+        };
+
+        Because b = () =>
+          spec.catch_exception(() => sut.shut_off());
+
+        It does_not_allow_them_to_shut_if_off = () =>
+          spec.exception_thrown.ShouldBeAn<SecurityException>();
+
+        static IPrincipal principal;
+      }
+    }
+
+    public class when_adding : concern_for_calculation
+    {
+      public class two_positive_numbers
+      {
         //Act
         Because b = () =>
           result = sut.add(2, 3);
@@ -46,7 +95,6 @@ namespace prep.test_examples
           command.received(x => x.ExecuteNonQuery());
 
         static int result;
-        static IDbCommand command;
       }
 
       public class a_negative_to_a_positive
@@ -56,9 +104,25 @@ namespace prep.test_examples
 
         It does_not_use_the_connection = () =>
           connection.never_received(x => x.Open());
-          
+
         It cannot_do_the_addition = () =>
           spec.exception_thrown.ShouldBeAn<ArgumentException>();
+      }
+
+      public class and_an_offset_is_specified
+      {
+        Establish c = () =>
+        {
+          depends.on(1);
+        };
+
+        Because b = () =>
+          result = sut.add(2, 3);
+
+        It applies_the_sum_with_the_offset = () =>
+          result.ShouldEqual(6);
+
+        static int result;
       }
     }
   }
